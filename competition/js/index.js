6993d6718f548843931cc77cc41f97b53957a274
Patrick McCorry <stonecoldpat@gmail.com>
2020-04-17 10:33:05 +0100

    new message board address

--- competition/js/index.js
+++ competition/js/index.js
@@ -1,4 +1,4 @@
-web3 = new Web3(Web3.givenProvider);
+var web3 = Web3 ? new Web3(Web3.givenProvider || "ws://localhost:8546") : null;
 
 var account;
 web3.eth.getAccounts().then((f) => {
@@ -9,9 +9,9 @@ abi = JSON.parse(
   `[{"inputs":[{"internalType":"string","name":"_msg","type":"string"}],"name":"setMessage","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"getLatestMessages","outputs":[{"components":[{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"blockno","type":"uint256"}],"internalType":"struct MessageBoard.Msg[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMessages","outputs":[{"components":[{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"blockno","type":"uint256"}],"internalType":"struct MessageBoard.Msg[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"msgList","outputs":[{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"blockno","type":"uint256"}],"stateMutability":"view","type":"function"}]`
 );
 contract = new web3.eth.Contract(abi);
-contract.options.address = "0xBb2fCC6727e4475F8843dE10289345D750AD1EfA";
+contract.options.address = "0x219E6C517D0d2fD0480cED38ceaDdA4707B99EAf";
 
-$(document).ready(function() {
+$(document).ready(function () {
   contract.methods
     .getLatestMessages(25)
     .call()
